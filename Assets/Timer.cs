632752229e94afb11a6d2771c4a097ea7f3001d9
ethanharris14 <475039@chrome.escg.ac.uk>
2019-11-28 16:17:52 +0000

    added my timer and a finish platform

---
+++ Assets/Timer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Text timerText;
+    public float StartTime;
+    private bool Finnished = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Finnished)
+            return;
+
+        float t = Time.time - StartTime;
+
+        string minuites = ((int)t / 60).ToString();
+        string seconds = (t % 60).ToString("f2");
+
+        timerText.text = minuites + ":" + seconds;
+
+
+    }
+    public void Finnish()
+    {
+        Finnished = true;
+        timerText.color = Color.yellow;
+    }
+
+}
---
+++ Assets/finish.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class finish : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnTriggerEnter (Collider other)
+    {
+        GameObject.Find("player").SendMessage("Finnish");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
